571a8436d13f688c4a253faa192c4293740cb1bf
sousuke0422 <sousuke20xx@gmail.com>
2024-02-17 00:46:06 +0900

    chore: replace repo

--- packages/frontend/src/pages/about-misskey.vue
+++ packages/frontend/src/pages/about-misskey.vue
@@ -34,10 +34,10 @@ SPDX-License-Identifier: AGPL-3.0-only
 							{{ i18n.ts._aboutMisskey.source }}
 							<template #suffix>Official Misskey GitHub</template>
 						</FormLink>
-						<FormLink to="https://github.com/akarinext/akariskey" external>
+						<FormLink to="https://github.com/TeamBlackCrystal/ayuskey" external>
 							<template #icon><i class="ti ti-code"></i></template>
 							{{ i18n.ts._aboutMisskey.source }}
-							<template #suffix>Akariskey GitHub</template>
+							<template #suffix>ayuskey GitHub</template>
 						</FormLink>
 						<FormLink to="https://crowdin.com/project/misskey" external>
 							<template #icon><i class="ti ti-language-hiragana"></i></template>
